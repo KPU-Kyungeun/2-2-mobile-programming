4c229178decad860bfd74f40cf1003cd72973437
박경은 <ruddms936@naver.com>
2020-10-20 05:06:21 +0900

    feat: 5주차 4~13 예제 코드 추가

---
+++ project5/src/main/java/com/cookandroid/project5/Example_5_13.java
@@ -0,0 +1,14 @@
+package com.cookandroid.project5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Example_5_13 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example_5_13);
+    }
+}
---
+++ project5/src/main/java/com/cookandroid/project5/Practice_5_4.java
@@ -0,0 +1,14 @@
+package com.cookandroid.project5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Practice_5_4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practice_5_4);
+    }
+}

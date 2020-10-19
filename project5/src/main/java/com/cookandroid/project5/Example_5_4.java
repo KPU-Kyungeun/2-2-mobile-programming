61aecf91dbd1f7c8a7ff52b97731703b4a74fecc
박경은 <ruddms936@naver.com>
2020-10-20 01:14:38 +0900

    feat: 5장 실습 코드 추가

--- project5/src/main/java/com/cookandroid/project5/Example_5_4.java
+++ project5/src/main/java/com/cookandroid/project5/Example_5_4.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Example_5_4 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_example_5_4);
     }
 }
--- project5/src/main/java/com/cookandroid/project5/Example_5_5.java
+++ project5/src/main/java/com/cookandroid/project5/Example_5_5.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Example_5_5 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_example_5_5);
     }
 }
